ea091f896201afdeb62f86f54b19470a384d856b
heyset <c.inaciomatheus@gmail.com>
2023-02-19 04:59:25 -0300

    adds chapter 5.1

---
+++ chapter_5/defining_and_instantiating_structs/src/main.rs
@@ -0,0 +1,24 @@
+#[derive(Clone)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: user1.username.clone(),
+        ..user1
+    };
+
+    println!("{}", user1.username);
+}
