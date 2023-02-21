a44587aadcfc9351a6087d8a33f11e69238eb511
heyset <c.inaciomatheus@gmail.com>
2023-02-21 04:13:09 -0300

    adds code for chapter 11.1

---
+++ chapter_11/how_to_write_tests/adder/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 7,
+            height: 6,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 7,
+            height: 6,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
