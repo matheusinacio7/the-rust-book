0513f76c47c31f08cfc86933d2e502db8ff12e3d
heyset <c.inaciomatheus@gmail.com>
2023-02-21 04:56:50 -0300

    adds reading file

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,7 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let search_pattern = args.get(1).expect("Must inform a search pattern");
     let file_path = args.get(2).expect("Must inform a file path");
+
+    let file_contents = fs::read_to_string(file_path).expect("Could not read file");
+    for (i, line) in file_contents.lines().enumerate() {
+        println!("Line {}: {}", i, line);
+    }
 }
