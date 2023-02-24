03da17052ba70ddcb4e34808cd188e4f1905d65f
heyset <c.inaciomatheus@gmail.com>
2023-02-24 03:15:33 -0300

    adds chapter 16.1

---
+++ chapter_16/using_threads_to_run_code_simultaneously/src/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    
+    println!("Vector from main thread: {:?}", v);
+    
+    handle.join().unwrap();
+}
